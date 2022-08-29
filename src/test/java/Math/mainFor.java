025f24004da9848f5119e6504f3f31c0976dfbae
sdous <marinavk1481@gmail.com>
2022-08-29 16:59:59 +0300

    practiseReverse

---
+++ src/test/java/Math/mainFor.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class mainFor {
+    public static void main(String[] args) {
+        for (int k = 100; k >= 0; k--) {
+            if (k % 4 == 0) {
+                System.out.println(k);
+            }
+        }
+    }
+}
